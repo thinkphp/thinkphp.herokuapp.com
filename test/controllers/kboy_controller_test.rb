53159979c644b49c53cfb0cd887b80659b603e36
Adrian Statescu <mergesortv@gmail.com>
2017-03-28 22:22:50 -0700

    rm

--- test/controllers/kboy_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class KboyControllerTest < ActionDispatch::IntegrationTest
-  test "should get homepage" do
-    get homepage_path
-    assert_response :success
-  end
-
-end
